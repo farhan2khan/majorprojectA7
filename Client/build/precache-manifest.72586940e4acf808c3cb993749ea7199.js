60748353bd587f3f32424997b3129cdb670f5271
Farhan Khan <Farhan2khank@gmail.com>
2022-06-30 20:33:28 +0530

    initial commit

---
+++ Client/build/precache-manifest.72586940e4acf808c3cb993749ea7199.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f437d47c79c708442e00f8288d28ca90",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73a602d5cf41fe94d044",
+    "url": "/static/css/main.9621e018.chunk.css"
+  },
+  {
+    "revision": "d5d9697360588dbdedae",
+    "url": "/static/js/2.184f16d7.chunk.js"
+  },
+  {
+    "revision": "0b4c429200d74988aa2f1657a298a43f",
+    "url": "/static/js/2.184f16d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73a602d5cf41fe94d044",
+    "url": "/static/js/main.b27306c0.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
---
+++ Client/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import Home from "./Home";
+import SignLanguageToSpeech from './SignLanguageToSpeech';
+import NotFound from './404';
+
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact><Home /></Route>
+        <Route path="/SignLanguageToSpeech" exact><SignLanguageToSpeech /></Route>
+        <Route path="/404" exact><NotFound /></Route>
+        <Redirect to="/404" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
